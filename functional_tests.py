1ba2f00c013c71360c73b679ec4021cbec3d39bf
Sanghyuk Park <sanghyuk.park85@gmail.com>
2024-09-27 14:17:47 -0500

    Refactor test code into unittest module

--- functional_tests.py
+++ functional_tests.py
@@ -2,34 +2,45 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 import unittest
 
-DRIVER_PATH = "/Users/sanghyuk/.local/bin/chromedriver"
-BRAVE_PATH = "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser"
 
-option = webdriver.ChromeOptions()
-option.binary_location = BRAVE_PATH
-service = Service(executable_path=DRIVER_PATH)
-browser = webdriver.Chrome(options=option, service=service)
+class NewVisitorTest(unittest.TestCase):
+    DRIVER_PATH = "/Users/sanghyuk/.local/bin/chromedriver"
+    BRAVE_PATH = "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser"
 
-# Edith has heard about a cool new online to-do app.
-# She goes to check out its homepage
-browser.get("http://localhost:8000")
+    def setUp(self):
+        option = webdriver.ChromeOptions()
+        option.binary_location = self.BRAVE_PATH
+        service = Service(executable_path=self.DRIVER_PATH)
 
-# She notices the page title and header mention to-do lists
-assert "To-Do" in browser.title
+        self.browser = webdriver.Chrome(options=option, service=service)
 
-# She is invited to enter a to-do item straight away
+    def tearDown(self):
+        self.browser.quit()
 
-# She types "Buy peacock feathers" into a text box
-# (Edith's hobby is tying fly-fishing lures)
+    def test_can_start_a_todo_list(self):
+        # Edith has heard about a cool new online to-do app.
+        # She goes to check out its homepage
+        self.browser.get("http://localhost:8000")
 
-# When she hits enter, the page updates, and now the page lists
-# "1: Buy peacock feathers" as an item in a to-do list
+        # She notices the page title and header mention to-do lists
+        self.assertIn("To-Do", self.browser.title)
 
-# There is still a text box inviting her to add another item.
-# She enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        # She is invited to enter a to-do item straight away
+        self.fail("Finish the test!")
 
-# The page updates again, and now shows both items on her list
+        # She types "Buy peacock feathers" into a text box
+        # (Edith's hobby is tying fly-fishing lures)
 
-# Satisfied, she goes back to sleep
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
 
-browser.quit()
+        # There is still a text box inviting her to add another item.
+        # She enters "Use peacock feathers to make a fly" (Edith is very methodical)
+
+        # The page updates again, and now shows both items on her list
+
+        # Satisfied, she goes back to sleep
+
+
+if __name__ == "__main__":
+    unittest.main()
