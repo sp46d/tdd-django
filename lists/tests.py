dc43ebfc1ff8aa263de61926bd46e2d1a5ea42b3
Sanghyuk Park <sanghyuk.park85@gmail.com>
2024-09-30 11:09:19 -0500

    Add functional tests to test for multiple item history

--- lists/tests.py
+++ lists/tests.py
@@ -1,10 +1,12 @@
-from django.http import HttpRequest
 from django.test import TestCase
-
-from lists.views import home_page
 
 
 class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get("/")
+        self.assertTemplateUsed(response, "lists/home.html")
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post("/", data={"item_text": "A new list item"})
+        self.assertContains(response, "A new list item")
         self.assertTemplateUsed(response, "lists/home.html")
