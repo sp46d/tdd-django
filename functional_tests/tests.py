7aff2cf04e577f824a4048998c1525ede04ef93f
Sanghyuk Park <sanghyuk.park85@gmail.com>
2024-10-01 10:48:34 -0500

    refactor functional tests to use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,12 +1,14 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
 import time
 import unittest
 
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
-class NewVisitorTest(unittest.TestCase):
+
+class NewVisitorTest(LiveServerTestCase):
     DRIVER_PATH = "/Users/sanghyuk/.local/bin/chromedriver"
     BRAVE_PATH = "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser"
 
@@ -19,7 +21,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
-        
+
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element(By.ID, "id_list_table")
         rows = table.find_elements(By.TAG_NAME, "tr")
@@ -28,7 +30,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_todo_list(self):
         # Edith has heard about a cool new online to-do app.
         # She goes to check out its homepage
-        self.browser.get("http://localhost:8000")
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn("To-Do", self.browser.title)
@@ -48,7 +50,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         self.check_for_row_in_list_table("1: Buy peacock feathers")
-        
+
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly"
         # (Edith is very methodical)
@@ -56,13 +58,9 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        
+
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table("1: Buy peacock feathers")
         self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
-        
+
         # Satisfied, she goes back to sleep
-
-
-if __name__ == "__main__":
-    unittest.main()
--- lists/tests.py
+++ lists/tests.py
@@ -6,20 +6,20 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "lists/home.html")
-        
+
     def test_displays_all_list_items(self):
         Item.objects.create(text="itemey 1")
         Item.objects.create(text="itemey 2")
         response = self.client.get("/")
         self.assertContains(response, "itemey 1")
         self.assertContains(response, "itemey 2")
-        
+
     def test_can_save_a_POST_request(self):
         self.client.post("/", data={"item_text": "A new list item"})
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, "A new list item")
-        
+
     def test_redirects_after_POST(self):
         response = self.client.post("/", data={"item_text": "A new list item"})
         self.assertRedirects(response, "/")
@@ -34,7 +34,7 @@ class ItemModelTest(TestCase):
         first_item = Item()
         first_item.text = "The first (ever) list item"
         first_item.save()
-        
+
         second_item = Item()
         second_item.text = "Item the second"
         second_item.save()
