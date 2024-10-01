83e9dfdc035697e41bc73fb4c425db8763d30561
Sanghyuk Park <sanghyuk.park85@gmail.com>
2024-10-01 10:15:43 -0500

    Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -6,7 +6,14 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "lists/home.html")
-
+        
+    def test_displays_all_list_items(self):
+        Item.objects.create(text="itemey 1")
+        Item.objects.create(text="itemey 2")
+        response = self.client.get("/")
+        self.assertContains(response, "itemey 1")
+        self.assertContains(response, "itemey 2")
+        
     def test_can_save_a_POST_request(self):
         self.client.post("/", data={"item_text": "A new list item"})
         self.assertEqual(Item.objects.count(), 1)
--- lists/views.py
+++ lists/views.py
@@ -7,9 +7,6 @@ def home_page(request):
     if request.method == "POST":
         Item.objects.create(text=request.POST["item_text"])
         return redirect("/")
-        
-    return render(
-        request,
-        "lists/home.html",
-        {"new_item_text": request.POST.get("item_text", "")}, # context variable
-    )
+    
+    items = Item.objects.all()
+    return render(request, "lists/home.html", {"items": items})
