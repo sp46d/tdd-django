869f4b9974e22d764827cb6780f14ffc43fee8cf
Sanghyuk Park <sanghyuk.park85@gmail.com>
2024-09-30 22:09:42 -0500

    refactor unittest and redirect after post request

--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from lists.models import Item
 
 
 # Create your views here.
 def home_page(request):
+    if request.method == "POST":
+        Item.objects.create(text=request.POST["item_text"])
+        return redirect("/")
+        
     return render(
         request,
         "lists/home.html",
