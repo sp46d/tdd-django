74cce15adf216f03e6e7ee2308f7ddf28c912a32
Sanghyuk Park <sanghyuk.park85@gmail.com>
2024-10-22 11:14:01 -0500

    set STATIC_ROOT in settings and disable admin

--- superlists/urls.py
+++ superlists/urls.py
@@ -21,7 +21,7 @@ from django.urls import include, path
 from lists import views as list_views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    # path("admin/", admin.site.urls),
     path("", list_views.home_page, name="home"),
     path("lists/", include("lists.urls")),
 ]
